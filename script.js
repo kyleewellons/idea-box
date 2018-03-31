94c7276b685055211e722c6d4b17dea5e4c651e4
KyleeC <kyleewellons@gmail.com>
2018-03-31 16:06:31 -0600

    Declare Jquery Variables

---
+++ script.js
@@ -0,0 +1,6 @@
+var ideaTitle = $('.main__input-title');
+var ideaBody = $('.main__input-body');
+var saveBtn = $('.main__button');
+var deleteBtn = $('.article__button-delete');
+var upVote = $('.upvote')
+var downVote = $('.downvote')
