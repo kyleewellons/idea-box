50b295f1923ab33ee948e98d8e0689afa51b5feb
KyleeC <kyleewellons@gmail.com>
2018-03-31 16:28:33 -0600

    Add template literal

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ var saveBtn = $('.main__button');
 var deleteBtn = $('.article__button-delete');
 var upVote = $('.upvote')
 var downVote = $('.downvote')
+var ideaSection = $('idea-section')
+
+ideaSection.prepend (`<article>
+        <h2 class="article__h2">${ideaTitle.val}</h2>
+        <button class="article__button-delete"></button>
+        <p>${ideaBody}</p>
+        <button class="article__button downvote"></button>
+        <button class="article__button upvote"></button>
+        <p class="article__p-quality">quality  
+          </p>
+        <p class="article__p-quality">: swill</p>
+        <hr>
+      </article>`)
