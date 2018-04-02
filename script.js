232bcf4aaf97eb4ed4236ee8c28ad132646f49a2
Keeliana Marler <kmmarler2@hotmail.com>
2018-04-02 12:45:16 -0600

    add comment

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-var ideaBody = $('.main__input-body');
-var saveBtn = $('.main__button');
-var deleteBtn = $('.article__button-delete');
-var upVote = $('.upvote')
-var downVote = $('.downvote')
-var ideaSection = $('idea-section')
+var cardTitle = $('.form__input-title');
+var cardBody = $('.form__input-body');
+var saveButton = $('.form__button');
+var deleteButton = $('.article__button-delete');
+var upVoteButton = $('.article__button-upvote');
+var downVoteButton = $('.article__button-downvote');
+var cardSection = $('.section__input');
 
-ideaSection.prepend (`<article>
-        <h2 class="article__h2">${ideaTitle.val}</h2>
-        <button class="article__button-delete"></button>
-        <p>${ideaBody}</p>
-        <button class="article__button downvote"></button>
-        <button class="article__button upvote"></button>
-        <p class="article__p-quality">quality  
-          </p>
-        <p class="article__p-quality">: swill</p>
-        <hr>
-      </article>`)
+saveButton.on('click', prependCard);
+
+$(document).ready(pullFromLocal);
+
+function prependCard() {
+    event.preventDefault();
+    cardSection.prepend(`
+        <article>
+            <h2 class="article__h2">${cardTitle.value}</h2>
+            <button class="article__button-delete"></button>
+            <p class="article__p-body">${cardBody.value}</p>
+            <button class="article__button article__button-downvote"></button>
+            <button class="article__button article__button-upvote"></button>
+            <p class="article__p-quality">quality</p>
+            <p class="article__p-quality">: swill</p>
+            <hr>
+        </article>`);
+}
+
