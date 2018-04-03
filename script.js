0c22582ffd2b7ad70b3af8982d25a652166ac585
Keeliana Marler <kmmarler2@hotmail.com>
2018-04-03 14:07:59 -0600

    add quality functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var cardTitle = $(".form__input-title")
 var cardBody = $(".form__input-body")
 
+$(".idea-cards").on('click','.article__button-delete', deleteCard);
+$('.form__button').on('click', saveButton);
+$('.idea-cards').on('click', '.article__button-upvote', upVote);
+$('.idea-cards').on('click', '.article__button-downvote', downVote);
+
 function Card(title, body, id, quality) {
   this.title = title;
   this.body = body;
@@ -18,15 +23,12 @@ function cardPrepend(newCard) {
         <p class="article__p-body">${newCard.body}</p>
         <button class="article__button article__button-downvote"></button>
         <button class="article__button article__button-upvote"></button>
-        <p class="quality">quality: </p>
+        <p class="quality">quality:\u00A0</p>
         <p class="quality article__p-quality">${newCard.quality}</p>
         <hr>
       </article>
       `);
 }
-
-
-$(".idea-cards").on('click','.article__button-delete', deleteCard)
 
 function deleteCard() {
   var card = this.closest("article").id;
@@ -34,7 +36,6 @@ function deleteCard() {
   this.closest("article").remove();
 }
 
-$('.form__button').on('click', saveButton)
 function saveButton(e) {
   e.preventDefault();
   var newCard = new Card(cardTitle.val(), cardBody.val());
@@ -49,8 +50,8 @@ function clearFields(){
 } 
 
 function addToLocal(newCard) {
-  var stringifyObj  = JSON.stringify(newCard);
-  localStorage.setItem(newCard.id, stringifyObj)
+  var stringifyObj = JSON.stringify(newCard);
+  localStorage.setItem(newCard.id, stringifyObj);
 }
 
 function pullFromLocal() {
@@ -59,20 +60,17 @@ function pullFromLocal() {
     var newCard = JSON.parse(getCard);
     cardPrepend(newCard)
   }
-
 }
 
-$(".idea-cards").on('click', '.article__button-upvote', upVote);
-$(".idea-cards").on('click', '.article__button-downvote', downVote);
-
-function upVote() {
-  var parentId = $(event.target).parent().attr('id');
-  var currentQuality = $(event.target).siblings('.article__p-quality');
-  if ($(currentQuality).text === 'swill') {
-    $(currentQuality).text = ('plausible');
-  } else if ($(currentQuality).text === 'plausible') {
-    $(currentQuality).text = ('GENIUS');
+function upVote(event) {
+  var parentId = $(event.target).parent().attr('id'); 
+  var currentQuality = $(event.target).siblings(".article__p-quality");
+  if (currentQuality.text() === 'swill') {
+    $(currentQuality).text('plausible');
+  } else if (currentQuality.text() === 'plausible') {
+    $(currentQuality).text('GENIUS');
   }
+  console.log(currentQuality);
   var parsedCard = JSON.parse(localStorage.getItem(parentId));
   parsedCard.quality = $(currentQuality).text();
   localStorage.setItem(parsedCard.id, JSON.stringify(parsedCard));
@@ -81,9 +79,8 @@ function upVote() {
 
 
 function downVote(event) {
-  var parentId = $(event.target).parent().attr("id")
+  var parentId = $(event.target).parent().attr("id");
   var currentQuality = $(event.target).siblings('.article__p-quality');
-  console.log(currentQuality);
   if ($(currentQuality).text() === 'GENIUS') {
     $(currentQuality).text('plausible')
   } else if ($(currentQuality).text() === 'plausible') {
